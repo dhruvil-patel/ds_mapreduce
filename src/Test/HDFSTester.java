38f87476b0c32fc6708c3430ad5ad1f9e0321823
Jigar Kaneriya <kaneriya_jigar@yahoo.com>
2015-03-14 15:12:01 +0530

    HDFSTest modiified for only read

--- src/Test/HDFSTester.java
+++ src/Test/HDFSTester.java
@@ -8,8 +8,10 @@ import API.HDFSAPI;
 
 public class HDFSTester {
 	public static void main(String args[]) throws NotBoundException, IOException{
-		HDFSAPI hdfs = new HDFSAPI("172.17.42.1",4096000);
-		hdfs.copyToHDFS(args[0], "hdfs_"+args[0]);
+		HDFSAPI hdfs = new HDFSAPI("10.42.0.1",4096000);
+
+		if(args.length < 2)
+			hdfs.copyToHDFS(args[0], "hdfs_"+args[0]);
 		for(String s: hdfs.listFiles("/"))
 			System.out.println(s);
 		System.out.println("Write done");
